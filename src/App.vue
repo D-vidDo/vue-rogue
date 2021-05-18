93340c1ed70f50d8ac1e4d8e1e2f27e01e399150
David Do <55934975+D-vidDo@users.noreply.github.com>
2021-05-17 23:28:48 -0600

    added heal cooldown, reset game method, cannot buy during battle

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,33 @@
     <v-main>
       <h1>Rogue Game</h1>
       <div>
-        gold: {{ gold }} crit chance: {{ critchance }} hp bonus: {{ hpBonus }} overheal: {{overheal}}
+        gold: {{ gold }} crit chance: {{ critchance }} hp bonus:
+        {{ hpBonus }} overheal: {{ overheal }}
         <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Cost</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in items" :key="item.name">
-                    <!-- <td>{{item.value}}</td> -->
-                    <td>{{item.name}}</td>
-                    <td>{{item.cost}}</td>
-                    <td><v-btn v-on:click="purchaseItem(item.cost, item.type, item.value), shopSnack()">Buy</v-btn></td>
-                </tr>
-
-            </tbody>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Cost</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in items" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.cost }}</td>
+              <td>
+                <v-btn
+                  v-on:click="
+                    purchaseItem(item.cost, item.type, item.value), shopSnack()
+                  "
+                  >Buy</v-btn
+                >
+              </td>
+            </tr>
+          </tbody>
         </table>
 
-
-<!--  -->
-
-
+        <!--  -->
 
         <Snackbar />
         <v-snackbar v-model="snackbar" timeout="2500">
@@ -69,21 +72,23 @@ export default {
     return {
       snackbar: "",
       timeout: 500,
+      inBattle: false,
       // base health
       enemyHP: 100,
       playerHP: 100,
       hpBonus: 0,
       overheal: false,
+      healcd: 5,
       // attack base damage stats
-      playerDamageMin: 1,
+      playerDamageMin: 5,
       playerDamageMax: 10,
       critchance: 0,
       // base heal stats
       playerHealMin: 5,
       playerHealMax: 20,
       // enemy base damage stats
-      enemyDamageMin: 0,
-      enemyDamageMax: 0,
+      enemyDamageMin: 2,
+      enemyDamageMax: 8,
       // gold
       gold: 0,
       // messages
@@ -96,15 +101,15 @@ export default {
       // shop
       items: [
         // health
-      { value: 10, name: 'HP +10', cost: 50, type: "hp" },
-      { value: 30, name: 'HP +30', cost: 75, type: "hp" },
-      { value: 50, name: 'HP +50', cost: 120, type: "hp" },
-      //crit
-      { value: 0.1, name: 'Crit +10%', cost: 150, type: "crit" },
-      { value: 0.25, name: 'Crit +25%', cost: 300, type: "crit" },
-      // overheal
-      {value: "overheal", name: 'Overheal', cost: 500, type: "item"},
-    ],
+        { value: 10, name: "HP +10", cost: 50, type: "hp" },
+        { value: 30, name: "HP +30", cost: 75, type: "hp" },
+        { value: 50, name: "HP +50", cost: 120, type: "hp" },
+        //crit
+        { value: 0.1, name: "Crit +10%", cost: 150, type: "crit" },
+        { value: 0.25, name: "Crit +25%", cost: 300, type: "crit" },
+        // overheal
+        { value: "overheal", name: "Overheal", cost: 500, type: "item" },
+      ],
     };
   },
 
@@ -122,6 +127,7 @@ export default {
         this.enemyHP -= dmg;
         this.playerMessage = "⚔️You attack for " + dmg + "⚔️";
       }
+      this.healcd--;
       if (this.checkWin()) {
         return;
       }
@@ -135,6 +141,7 @@ export default {
       this.enemyMessage = "⚔️The enemy attacks for " + dmg + "⚔️";
       this.playerHP -= dmg;
       this.checkWin();
+      this.inBattle = true;
       return;
     },
     calculateDamage(min, max) {
@@ -142,13 +149,23 @@ export default {
     },
     heal() {
       let heal = this.calculateDamage(this.playerHealMin, this.playerHealMax);
-
-      if (!this.overheal) {
-        if (this.playerHP + heal > (this.playerHP + this.hpBonus)) {
-          this.playerHP = 100 + this.hpBonus;
+      if (this.healcd <= 0) {
+        if (!this.overheal) {
+          if (this.playerHP + heal >= 100 + this.hpBonus) {
+            this.playerHP = 100 + this.hpBonus;
+            this.healcd = 5;
+          } else {
+            this.playerHP += heal;
+            this.healcd = 5;
+          }
+        } else {
+          this.playerHP += heal;
+          this.healcd = 5;
         }
       } else {
-        this.playerHP += heal;
+        this.playerMessage =
+          "Heal is on Cool Down! " + this.healcd + " turn(s) left!";
+        return;
       }
 
       this.playerMessage = "🩹You heal for " + heal + "🩹";
@@ -159,39 +176,70 @@ export default {
     },
     checkWin() {
       if (this.enemyHP <= 0) {
-        confirm("You slaughtered the enemy... A stronger one approaches");
-        this.goldScaling += 25;
-        this.hpScaling += 25;
-        this.gold += 25 + this.goldScaling;
+        confirm("You slaughtered the enemy... A stronger one approaches\nAtk +5");
         this.nextEnemy();
         return true;
       } else if (this.playerHP <= 0) {
         confirm("You lose!");
+        this.resetGame();
       }
+    },
+    resetGame() {
+      // base health
+      this.enemyHP = 100;
+      this.playerHP = 100;
+      this.hpBonus = 0;
+      this.overheal = false;
+      this.healcd = 5;
+      // attack base damage stats
+      this.playerDamageMin = 1;
+      this.playerDamageMax = 10;
+      this.critchance = 0;
+      // base heal stats
+      this.playerHealMin = 5;
+      this.playerHealMax = 20;
+      // enemy base damage stats
+      this.enemyDamageMin = 1;
+      this.enemyDamageMax = 10;
+      // gold
+      this.gold = 0;
     },
     nextEnemy() {
       this.playerHP = 100 + this.hpBonus;
       this.enemyHP = 100 + this.hpScaling;
+      this.goldScaling += 25;
+      this.hpScaling += 25;
+      this.enemyDamageMin += 2;
+      this.enemyDamageMax += 5;
+      this.playerDamageMin += 2;
+      this.playerDamageMax += 5;
+      this.healcd = 5;
+      this.gold += 25 + this.goldScaling;
+      this.inBattle = false;
     },
     purchaseItem(itemCost, itemStat, itemVal) {
-      if (itemCost > this.gold) {
-        this.message = "Not Enough Gold!";
+      if (this.inBattle) {
+        this.message="Currently in battle! Can't buy items.";
       } else {
-        if (itemStat == "hp") {
-          this.hpBonus += itemVal;
-          this.playerHP = 100 + this.hpBonus;
-          this.gold -= itemCost;
-          this.message = "Item Bought!";
-        } else if (itemStat == "crit") {
-          this.critchance += itemVal;
-          this.gold -= itemCost;
-          this.message = "Item Bought!";
-        } else if (itemStat == "item") {
-          if (itemVal=="overheal"){
-            this.overheal = true;
+        if (itemCost > this.gold) {
+          this.message = "Not Enough Gold!";
+        } else {
+          if (itemStat == "hp") {
+            this.hpBonus += itemVal;
+            this.playerHP = 100 + this.hpBonus;
+            this.gold -= itemCost;
+            this.message = "Item Bought!";
+          } else if (itemStat == "crit") {
+            this.critchance += itemVal;
+            this.gold -= itemCost;
+            this.message = "Item Bought!";
+          } else if (itemStat == "item") {
+            if (itemVal == "overheal") {
+              this.overheal = true;
+            }
+            this.gold -= itemCost;
+            this.message = "Item Bought!";
           }
-          this.gold -= itemCost;
-          this.message = "Item Bought!";
         }
       }
     },
